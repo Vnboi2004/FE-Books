d1f8de2e922cd1a61f41a2ceb8c9cb465af93051
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2025-04-21 20:40:14 +0700

    change ui/ux of component home.tsx with section categori technology

--- src/components/BookSections/TechnologySection.tsx
+++ src/components/BookSections/TechnologySection.tsx
@@ -58,13 +58,13 @@ const TechnologySection: React.FC<TechnologySectionProps> = ({ subject = 'techno
                     <div className="flex items-center gap-2">
                         <button 
                             onClick={() => swiperRef.current?.slidePrev()}
-                            className="p-2 rounded-full bg-[#3E92CC] cursor-pointer group">
-                            <FiChevronLeft className="text-white group-hover:-translate-x-1 duration-300 transition-transform ease-in-out"/>
+                            className="p-2 rounded-full bg-primary cursor-pointer group">
+                            <FiChevronLeft className="text-on-primary group-hover:-translate-x-1 duration-300 transition-transform ease-in-out"/>
                         </button>
                         <button 
                             onClick={() => swiperRef.current?.slideNext()}
-                            className="p-2 rounded-full bg-[#3E92CC] cursor-pointer group">
-                            <FiChevronRight className="text-white group-hover:translate-x-1 duration-300 transition-transform ease-in-out"/>
+                            className="p-2 rounded-full bg-primary cursor-pointer group">
+                            <FiChevronRight className="text-on-primary group-hover:translate-x-1 duration-300 transition-transform ease-in-out"/>
                         </button>
                     </div>
                 </div>
@@ -86,20 +86,19 @@ const TechnologySection: React.FC<TechnologySectionProps> = ({ subject = 'techno
                         >
                             {books.map((book) => (
                                 <SwiperSlide key={book.id}>
-                                    <div className="w-full border border-gray-300 rounded-sm bg-[#3E92CC]/15 relative cursor-pointer">
-                                        <TiPinOutline className="absolute -top-1 -right-1 text-2xl"/>
-                                        <div className="p-4 w-full flex flex-col border-4 border-white">
+                                    <div className="w-full border border-outline-variant rounded-xl cursor-pointer hover:bg-primary-container hover:no-underline transition-all duration-200 ease-in-out active:rounded-sm">
+                                        <div className="p-4 w-full flex flex-col">
                                             {/* image */}
                                             <img
                                                 src={book.volumeInfo.imageLinks?.thumbnail || ''}   
                                                 alt={book.volumeInfo.title}
-                                                className="h-56 object-cover rounded-md mb-2"
+                                                className="h-56 object-cover rounded-xl mb-2"
                                             />
                                             <div className="flex flex-col items-start gap-1">
                                                 {/* Title */}
-                                                <h3 className="font-semibold line-clamp-1">{book.volumeInfo.title}</h3>
+                                                <h3 className="font-semibold text-inverse-surface line-clamp-1">{book.volumeInfo.title}</h3>
                                                 {/* authors */}
-                                                <p className="text-sm text-gray-600 line-clamp-1">
+                                                <p className="text-sm text-on-surface-variant line-clamp-1">
                                                     {book.volumeInfo.authors?.join(', ') || 'Tác giả không rõ'}
                                                 </p>
                                                 {/* published date */}
@@ -108,10 +107,10 @@ const TechnologySection: React.FC<TechnologySectionProps> = ({ subject = 'techno
                                                 <div className='flex items-center gap-2'>
                                                     <div className='flex items-center'>
                                                         {[...Array(5)].map((_, index) => (
-                                                            <TiStarFullOutline key={index} className={` ${index < book.volumeInfo.averageRating ? "text-yellow-400" : "text-gray-300"}`}/>
+                                                            <TiStarFullOutline key={index} className={` ${index < book.volumeInfo.averageRating ? "text-yellow-400" : "text-on-surface-variant"}`}/>
                                                         ))}
                                                     </div>
-                                                    <span className='text-sm text-gray-600'>({book.volumeInfo.averageRating ? book.volumeInfo.averageRating : 0})</span>
+                                                    <span className='text-sm text-on-surface-variant'>({book.volumeInfo.averageRating ? book.volumeInfo.averageRating : 0})</span>
                                                 </div>
                                             </div>
                                         </div>
