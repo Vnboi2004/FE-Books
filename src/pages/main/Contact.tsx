78fbd886af66dd209c2d6742389a2109a7f3813d
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2025-04-26 13:59:36 +0700

    change layout contact

--- src/pages/main/Contact.tsx
+++ src/pages/main/Contact.tsx
@@ -2,46 +2,38 @@ import MainLayout from '../../components/layout/MainLayout'
 
 const Contact = () => {
     return (
-        <MainLayout>
-            <div>
-                <h1 className='py-16 text-5xl text-on-surface text-center font-bold'>Liên hệ với chúng tôi</h1>
-                <form className="space-y-6 bg-white shadow-md rounded-xl p-8">
+      <MainLayout>
         <div>
-          <label className="block text-gray-700 font-semibold mb-2">Họ và tên</label>
-          <input
-            type="text"
-            placeholder="Nhập họ tên"
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
-        </div>
-
-        <div>
-          <label className="block text-gray-700 font-semibold mb-2">Email</label>
-          <input
-            type="email"
-            placeholder="example@email.com"
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
-        </div>
-
-        <div>
-          <label className="block text-gray-700 font-semibold mb-2">Nội dung</label>
-          <textarea
-            placeholder="Nhập nội dung liên hệ..."
-            rows={5}
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700 transition"
-        >
-          Gửi liên hệ
-        </button>
-      </form>
+          <h1 className='py-16 text-5xl text-on-surface text-center font-bold'>Liên hệ với chúng tôi</h1>
+          <div className='relative'>
+            {/* Content left */}
+            <div className='absolute top-0 left-0 bg-surface-1 w-full h-full md:w-[calc(50%+32px)]'>
+              <div>
+                {/* Logo */}
+                <img src="" alt="" />
+                {/* Text */}
+                <div>
+                  {/* Title */}
+                  <h1></h1>
+                  {/* Text content */}
+                  <h2></h2>
+                  {/* Description */}
+                  <p></p>
+                </div>
+              </div>
             </div>
-        </MainLayout>
+            {/* Content right */}
+            <div className='max-w-[740px] md:w-1/2 h-full bg-surface-2'>
+              <div>
+                {/* Title */}
+                <h1></h1>
+                {/* Info contact us */}
+                
+              </div>
+            </div>
+          </div>
+        </div>        
+      </MainLayout>
     )
 }
 
