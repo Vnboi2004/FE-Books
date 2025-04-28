63b33104ac8f6ac5a71367372c87e788f3b76968
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2025-04-28 22:16:24 +0700

    add page Contact focus completed slider with content contact info of me

--- src/pages/main/Contact.tsx
+++ src/pages/main/Contact.tsx
@@ -37,51 +37,51 @@ const Contact = () => {
               </div>
             </div>
             {/* Content right */}
-            <div className='absolute top-0 right-0 max-w-[740px] md:w-1/2 h-auto bg-surface-2 z-[1] rounded-3xl'>
-              <div className=''>
-                <Swiper
-                  effect={'cards'}
-                  grabCursor={true}
-                  modules={[EffectCards]}
-                  className="mySwiper"
-                >
-                  {/* Card 1 */}
-                  <SwiperSlide>
-                    <h1 className='text-5xl text-on-surface text-center font-bold py-16'>Liên hệ với chúng tôi</h1>
-                    <div className='flex flex-col gap-4 p-4'>
-                      {/* Box 1 */}
-                      <div className='flex items-center gap-4'>
-                        <div className='flex items-center gap-2 border px-6 py-10 border-outline-variant rounded-2xl w-full'>
-                          <FaPhoneVolume/>
-                          <span>+84 918 488 431</span>
-                        </div>
-                        <div className='flex items-center gap-2 border px-6 py-10 border-outline-variant rounded-2xl w-full'>
-                          <MdEmail/>
-                          <span>tphung1724@gmail.com</span>
-                        </div>
+            <div className='absolute -top-6 right-0 max-w-[740px] md:w-1/2 h-auto bg-surface-2 z-[1] rounded-3xl'>
+              <Swiper
+                effect={'cards'}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="mySwiper"
+              >
+                {/* Card 1 */}
+                <SwiperSlide className='py-8'>
+                  <h1 className='text-5xl text-on-surface text-center font-bold py-10'>Contact us</h1>
+                  <div className='flex flex-col gap-4 px-8 -translate-x-2'>
+                    {/* Box 1 */}
+                    <div className="flex gap-4">
+                      <div className="flex basis-1/2 flex-none items-center gap-2 border px-4 py-10 border-outline-variant rounded-2xl">
+                        <FaPhoneVolume className='text-primary text-lg'/>
+                        <span className='text-on-surface text-sm'>+84 918 488 431</span>
                       </div>
-                      {/* Box 2 */}
-                      <div className='flex items-center gap-4'>
-                        <div className='flex items-center gap-2 border px-6 py-10 border-outline-variant rounded-2xl w-full'>
-                          <BiLogoInstagramAlt  />
-                          <span>phuochung_4</span>
-                        </div>
-                        <div className='flex items-center gap-2 border px-6 py-10 border-outline-variant rounded-2xl w-full'>
-                          <FaFacebookF/>
-                          <span>Trương Phước Hưng</span>
-                        </div>
+                      <div className="flex basis-1/2 flex-none items-center gap-2 border px-4 py-10 border-outline-variant rounded-2xl">
+                        <MdEmail className='text-primary text-lg'/>
+                        <span className='text-on-surface text-sm'>tphung1724@gmail.com</span>
                       </div>
                     </div>
-                  </SwiperSlide>
-                  {/* Card 2 */}
-                  <SwiperSlide>Google map</SwiperSlide>
-                  {/* Card 3 */}
-                  <SwiperSlide>Get started</SwiperSlide>
-                </Swiper>
+                    {/* Box 2 */}
+                    <div className='flex gap-4'>
+                      <div className='flex basis-1/2 flex-none items-center  gap-2 border px-4 py-10 border-outline-variant rounded-2xl'>
+                        <BiLogoInstagramAlt className='text-primary text-lg'/>
+                        <span className='text-on-surface text-sm'>phuochung_4</span>
+                      </div>
+                      <div className='flex basis-1/2 flex-none items-center  gap-2 border px-4 py-10 border-outline-variant rounded-2xl'>
+                        <FaFacebookF className='text-primary text-lg'/>
+                        <span className='text-on-surface text-sm'>Trương Phước Hưng</span>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Note if spam messages*/}
+                  <p className='py-10 text-sm text-center text-on-surface-variant'>Please only contact us if you have a clear purpose. We do not accept spam, advertisements, or unrelated messages</p>
+                </SwiperSlide>             
+                {/* Card 2 */}
+                <SwiperSlide>Google map</SwiperSlide>
+                {/* Card 3 */}
+                <SwiperSlide>Get started</SwiperSlide>
+              </Swiper>
               </div>
             </div>
-          </div>
-        </div>        
+          </div>       
       </MainLayout>
     )
 }
