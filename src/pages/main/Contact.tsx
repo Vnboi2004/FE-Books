be7c72e9e0bf2b620cf720334cc55b104eccf8d6
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2025-04-28 23:43:14 +0700

    add page Contact focus completed slider with content contact address

--- src/pages/main/Contact.tsx
+++ src/pages/main/Contact.tsx
@@ -37,16 +37,17 @@ const Contact = () => {
               </div>
             </div>
             {/* Content right */}
-            <div className='absolute -top-6 right-0 max-w-[740px] md:w-1/2 h-auto bg-surface-2 z-[1] rounded-3xl'>
+            <div className='absolute -top-6 right-0 max-w-[740px] md:w-1/2 h-full z-[1]'>
               <Swiper
                 effect={'cards'}
                 grabCursor={true}
                 modules={[EffectCards]}
-                className="mySwiper"
+                className="mySwiper w-full h-full"
               >
                 {/* Card 1 */}
-                <SwiperSlide className='py-8'>
-                  <h1 className='text-5xl text-on-surface text-center font-bold py-10'>Contact us</h1>
+                <SwiperSlide className='py-8 h-full bg-surface-2 rounded-3xl'>
+                  {/* Title */}
+                  <h1 className='text-5xl text-on-surface text-center font-bold py-10'>Git in Touch</h1>
                   <div className='flex flex-col gap-4 px-8 -translate-x-2'>
                     {/* Box 1 */}
                     <div className="flex gap-4">
@@ -75,9 +76,31 @@ const Contact = () => {
                   <p className='py-10 text-sm text-center text-on-surface-variant'>Please only contact us if you have a clear purpose. We do not accept spam, advertisements, or unrelated messages</p>
                 </SwiperSlide>             
                 {/* Card 2 */}
-                <SwiperSlide>Google map</SwiperSlide>
+                <SwiperSlide className='py-8 h-full bg-surface-2 rounded-3xl'>
+                  {/* Title */}
+                  <h1 className='text-5xl text-on-surface text-center font-bold pt-10'>Find Us Here</h1>
+                  {/* Description */}
+                  <p className='text-sm text-on-surface-variant text-center py-6'>Visit our office or reach out to us through the map below.</p>
+                  {/* Google maps */}
+                  <div className="px-8">
+                    <div className="w-full h-[300px] rounded-2xl overflow-hidden shadow-lg">
+                      <iframe
+                        title="Google Map"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.685772993273!2d106.75874497451784!3d10.835341558105318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527ae7b121297%3A0x9fda1a8492c5074d!2zNDgvMy8zQSDEkC4gU-G7kSAzLCBUcsaw4budbmcgVGjhu40sIFRo4bunIMSQ4bupYywgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1745858378946!5m2!1svi!2s"
+                        width="100%"
+                        height="100%"
+                        style={{ border: 0 }}
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                      />
+                    </div>
+                  </div>
+                </SwiperSlide>
                 {/* Card 3 */}
-                <SwiperSlide>Get started</SwiperSlide>
+                <SwiperSlide className='py-8 h-full bg-surface-2 rounded-3xl'>
+                  <h1 className='text-5xl text-on-surface text-center font-bold py-10'>Send Us a Message</h1>
+                </SwiperSlide>
               </Swiper>
               </div>
             </div>
